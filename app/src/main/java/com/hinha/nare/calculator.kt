0719f9b3c7bc3970c7c4b9cf623b33d429be0f3e
HinhaOGE <martinuz.dawan9@gmail.com>
2019-10-06 10:25:18 +0700

    init layout

---
+++ app/src/main/java/com/hinha/nare/calculator.kt
@@ -0,0 +1,12 @@
+package com.hinha.nare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class calculator : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
